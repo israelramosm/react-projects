a6370fece6d27c74832a1751520410a84016e2f2
Israel-94 <arnulfo_israel@hotmail.com>
2019-05-22 22:01:14 -0500

    moving folders and files part II

--- book-list/src/actions/index.js
+++
@@ -1,8 +0,0 @@
-export function selectBook(book) {
-  // selectedBook is an ActionCreator, It needs to return an Actio,
-  // an object with type property
-  return {
-    type: "BOOK_SELECTED",
-    payload: book
-  };
-}
