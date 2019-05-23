8e6c22a39f2a898872fd877dca60fcf36bc8905e
Israel-94 <arnulfo_israel@hotmail.com>
2019-05-22 22:15:38 -0500

    working for now

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import App from './components/app';
+import App from "./components/app";
 // import reducers from './reducers';
-const reducers;
+// TODO: custom reducer for now
+const reducers = () => {};
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-        <Router>
-            <div>
-                <Switch>
-                    <Route path="/post/new" component={PostNew} />
-                    <Route path="/post/:id" component={PostShow} />
-                    <Route path="/" component={App} />
-                </Switch>
-            </div>
-        </Router>
-    </Provider>,
-    document.getElementById('root'));
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" component={App} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
